d572db125dd365ef41035215ddd3d8b2610999b2
Rahul <rahulsuthar1329@gmail.com>
2024-07-13 02:22:58 +0530

    imageUrl issue resolved

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { X } from "lucide-react";
 import Image from "next/image";
@@ -7,51 +7,45 @@ import { UploadDropzone } from "@/lib/uploadthing";
 
 import "@uploadthing/react/styles.css";
 
-interface FileUploadProps{
-    onChange: (url?: string) => void;
-    value: string;
-    endpoint: "messageFile" | "serverImage"
+interface FileUploadProps {
+  onChange: (url?: string) => void;
+  value: string;
+  endpoint: "messageFile" | "serverImage";
 }
 
-export const FileUpload = ({
-    onChange,
-    value, 
-    endpoint
-}: FileUploadProps) => {
-    const fileType = value?.split(".").pop();
+export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
+  const fileType = value?.split(".").pop();
 
-    if(value && fileType !=="pdf"){
-        return(
-            <div className="relative h-20 w-20">
-                <Image
-                fill
-                src={value}
-                alt="Upload"
-                className="rounded-full"
-                />
-
-                <button
-                onClick={() => onChange("")}
-                className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
-                type="button"
-                >
-                    <X className="h-4 w-4" />
-                </button>
-            </div>
-        )
-    }
-
-    return(
-        <UploadDropzone
-            endpoint ={ endpoint }
-            onClientUploadComplete = {(res) => {
-                    onChange(res?.[0].url);
-                }}
-                onUploadError ={(error: Error) => {
-                    console.log(error);
-                }}
+  if (value && fileType !== "pdf") {
+    return (
+      <div className="relative h-20 w-20">
+        <Image
+          fill
+          src={value}
+          alt="Upload"
+          className="rounded-full"
         />
-        
-    )
-}
 
+        <button
+          onClick={() => onChange("")}
+          className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
+
+  return (
+    <UploadDropzone
+      endpoint={endpoint}
+      onClientUploadComplete={(res) => {
+        onChange(res?.[0].url);
+      }}
+      onUploadError={(error: Error) => {
+        console.log(error);
+      }}
+    />
+  );
+};
--- components/models/initial-model.tsx
+++ components/models/initial-model.tsx
@@ -1,5 +1,4 @@
-"use client"
-
+"use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import axios from "axios";
@@ -7,143 +6,153 @@ import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 
-
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
 } from "@/components/ui/dialog";
 
 import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
 } from "@/components/ui/form";
 
 import { FileUpload } from "@/components/file-upload";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 const formSchema = z.object({
-    name: z.string().min(1, {
-        message: "Server image is required."
-
-    })
+  name: z.string().min(1, {
+    message: "Server image is required.",
+  }),
 });
 
-export const InitialModel = ( ) => {
+export const InitialModel = () => {
+  const [isMounted, setIsMounted] = useState(false);
+  const [imageUrl, setImageUrl] = useState<string>();
 
-    const [isMounted, setIsMounted] = useState(false);
+  const router = useRouter();
 
-    const router = useRouter();
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
-    useEffect(() => {
-        setIsMounted(true); 
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      imageUrl: "",
+    },
+  });
 
-    }, []);
+  const isLoading = form.formState.isSubmitting;
 
-    const form = useForm({
-        resolver: zodResolver (formSchema),
-        defaultValues: {
-            name: "",
-            imageUrl: "",
-        }
-    });
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      console.log({ values });
 
-    const isLoading = form.formState.isSubmitting;
-
-    const onSubmit = async (values: z.infer<typeof formSchema>) => {
-
-      try{
-        await axios.post("/api/server",values);
-
-        form.reset();
-        router.refresh();
-        window.location.reload();
+      const payload = {
+        name: values.name,
+        imageUrl
       }
-      catch (error) {
-        console.log(error); 
-      }
+      const response = await axios.post("/api/server", payload);
 
+      console.log("resp", response.data);
+
+      form.reset();
+      router.refresh();
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    if(!isMounted){
-        return null;
+  if (!isMounted) {
+    return null;
+  }
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+
+    if (file) {
+      const url = URL.createObjectURL(file);
+      setImageUrl(url);
+    } else {
+      setImageUrl("");
     }
+  };
 
-    return (
-       <Dialog open>
-            <DialogContent className="bg-white text-black p-0 overflow-hidden">
-                <DialogHeader className="pt-8 px-6">
-                    <DialogTitle className="text-2xl text-center font-bold">
-                        Customize your server
-                    </DialogTitle>
-                    <DialogDescription className="text-center text-zinc-500">
-                        Give your server a personality with a name and an image. You can always change it later.
-                    </DialogDescription>
-                </DialogHeader>
-                <Form{...form}>
-                    <form onSubmit={form.handleSubmit(onSubmit)} 
-                    className="space-y-8">
-                        <div className="space-y-8 px-6">
-                            <div className="flex items-center justify-center text-center">
-                               <FormField
-                               control = {form.control}
-                               name= "imageUrl"
-                               render={({field}) => (
-                                <FormItem>
-                                    <FormControl>
-                                        <FileUpload 
-                                        endpoint="serverImage"
-                                        value={field.value}
-                                        onChange={field.onChange}
-                                        />
-                                    </FormControl>
-                                </FormItem>
-                               )}
-                               />
-                            </div>
+  return (
+    <Dialog open>
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold">
+            Customize your server
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            Give your server a personality with a name and an image. You can
+            always change it later.
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <div className="space-y-8 px-6">
+              <div className="flex items-center justify-center text-center">
+                {!imageUrl ? (
+                  <input
+                    type="file"
+                    value={imageUrl}
+                    onChange={handleFileChange}
+                  />
+                ) : (
+                  <Image
+                    src={imageUrl}
+                    width={50}
+                    height={50}
+                    alt="selected images"
+                  />
+                )}
+              </div>
 
-                            <FormField
-                                control={form.control}
-                                name="name"
-                                render={({field})=> (
-                                    <FormItem>
-                                        <FormLabel
-                                            className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70"
-                                            >
-                                                Server name
-                                        </FormLabel>
-                                        <FormControl>
-                                            <Input
-                                            disabled = {isLoading}
-                                            className="bg-zinc-300/50 border-0
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
+                      Server name
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={isLoading}
+                        className="bg-zinc-300/50 border-0
                                             focus-visible:ring-0 text-black focus-visible:ring-offset-0"
-                                            placeholder="Enter server name"
-                                            {...field}
-                                            />
-                                        </FormControl>
-                                        <FormMessage />
-                                    </FormItem>
-                                )}
-                            />
-                        </div>
-                        <DialogFooter className="bg-gray-100 px-6 py-4">
-                            <Button variant="primary" disabled={isLoading}>
-                                Create
-                            </Button>
-
-                        </DialogFooter>
-                    </form>
-                </Form>
-            </DialogContent>
-       </Dialog>
-    )
-}
+                        placeholder="Enter server name"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <DialogFooter className="bg-gray-100 px-6 py-4">
+              <Button variant="primary" disabled={isLoading}>
+                Create
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
